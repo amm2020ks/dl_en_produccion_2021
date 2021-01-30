d25f0ae1b533a3973e82ae2d7e0e7457f5fab928
ksclase <ksclase@gmail.com>
2021-01-30 08:55:59 +0000

    Script to trigger hypertuning

---
+++ bin/training_hyper.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+gcloud ai-platform jobs submit training mnist_amm_ht_`date +"%s"` \
+  --python-version 3.7 \
+  --runtime-version 2.3 \
+  --scale-tier BASIC \
+  --package-path ./trainer \
+  --module-name trainer.task \
+  --region europe-west1 \
+  --job-dir gs://amm-20200123-kschool/tmp \
+  --config ./bin/hyper.yaml
+  -- \
+  --model-output-path gs://amm-20200123-kschool/models \
+  --hypertune
