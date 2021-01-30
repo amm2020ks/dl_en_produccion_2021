73b6c2dc194097a46a0ca79228cb51b3a4cf2304
ksclase <ksclase@gmail.com>
2021-01-30 08:58:10 +0000

    small bug

--- bin/training_hyper.sh
+++ bin/training_hyper.sh
@@ -9,7 +9,7 @@ gcloud ai-platform jobs submit training mnist_amm_ht_`date +"%s"` \
   --module-name trainer.task \
   --region europe-west1 \
   --job-dir gs://amm-20200123-kschool/tmp \
-  --config ./bin/hyper.yaml
+  --config ./bin/hyper.yaml \
   -- \
   --model-output-path gs://amm-20200123-kschool/models \
   --hypertune
